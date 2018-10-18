5e1ed221279e4a843d96ad5cb52f4ff6f94f8fb0
eooeoonin <22430218@qq.coom>
2018-10-18 15:08:49 +0800

    init2

--- 1013/1017.py
+++ 1013/1017.py
@@ -1,10 +1,22 @@
 from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
 
 caps = {}
 caps["appActivity"] = "com.dkkj.modules.SplashPage"
 caps["appPackage"] = "com.citiccard.mobilebank"
 caps["platformName"] = "Android"
-caps["deviceName"] = "111"
+caps["deviceName"] = "127.0.0.1:62001"
+caps["noReset"] = True
 
 driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
 driver.implicitly_wait(5)
+
+driver.find_element_by_xpath("//android.widget.TextView[@text='积分']").click()
+driver.find_element_by_xpath("//android.widget.ImageView[@instance=8]").click()
+TouchAction(driver).press(x=300, y=2400).move_to(x=300, y=300).release().perform()
+driver.find_element_by_xpath("(//android.view.View[@content-desc='Link'])[2]").click()
+
+x = driver.get_window_size()['width']
+y = driver.get_window_size()['height']
+driver.swipe()
+
